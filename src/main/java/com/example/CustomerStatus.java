f85cbcf0b1e19d5b4eef0f48057db5b1827cd25d
Administrator <daniel.paez@apotek1.no>
2016-10-07 14:42:32 +0200

    Added Vaadin demo files

---
+++ src/main/java/com/example/CustomerStatus.java
@@ -0,0 +1,5 @@
+package com.example;
+
+public enum CustomerStatus {
+    ImportedLead, NotContacted, Contacted, Customer, ClosedLost
+}
--- src/main/java/com/example/MyVaadinUI.java
+++ src/main/java/com/example/MyVaadinUI.java
@@ -1,14 +1,36 @@
 package com.example;
 
+import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.Label;
+import com.vaadin.ui.Grid;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+import java.util.List;
 
 @SpringUI
-public class MyVaadinUI extends UI {
+ public class MyVaadinUI extends UI {
+    private CustomerService service = CustomerService.getInstance();
+    private Grid grid = new Grid();
+
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        setContent(new Label("Hello! test I'm the root UI!"));
+        final VerticalLayout layout = new VerticalLayout();
+
+        grid.setColumns("firstName", "lastName", "email");
+        // add Grid to the layout
+        layout.addComponent(grid);
+
+        updateList();
+
+        layout.setMargin(true);
+        setContent(layout);
+    }
+
+    public void updateList() {
+        // fetch list of Customers from service and assign it to Grid
+        List<Customer> customers = service.findAll();
+        grid.setContainerDataSource(new BeanItemContainer<>(Customer.class, customers));
     }
 }
